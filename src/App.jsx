c4d7234927d85c120256ddb2921f8b08cc47b9c6
Aurélien St <aurelien.strimelle@bstorm.be>
2025-09-08 10:46:50 +0200

    Protected route 👮‍♂️

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ import LoginPage from './pages/auth/login.page';
 import RegisterPage from './pages/auth/register.page';
 import GuestBookPage from './pages/guestbook/guestbook.page';
 import GuestBookDetailPage from './pages/guestbook/guestbook-detail.page';
+import { useAtomValue } from 'jotai';
+import { tokenAtom } from './atoms/token.atom.js';
+import { Navigate } from 'react-router';
+
+function Protected({element}) {
+  const token = useAtomValue(tokenAtom);
+
+  if(!token) {
+    return <Navigate to='/login' />
+  }
+  return element;
+}
 
 function App() {
 
@@ -19,7 +31,7 @@ function App() {
           <Route path="register" element={<RegisterPage />} />
           <Route path="guestbook">
             <Route index element={<GuestBookPage />} />
-            <Route path=":id" element={<GuestBookDetailPage />} />
+            <Route path=":id" element={<Protected element={<GuestBookDetailPage />} />} />
           </Route>
         </Routes>
       </main>
